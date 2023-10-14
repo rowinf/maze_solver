38c5863c532f8aa6d83570b2f61835fee81c8f1a
rowinf <robert.irwin@outlook.co.nz>
2023-10-14 20:53:01 +1300

    refactor with draw_wall

--- maze_solver.py
+++ maze_solver.py
@@ -1,11 +1,6 @@
 from tkinter import Tk, BOTH, Canvas
 
 
-class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
 class Cell:
     def __init__(self, win, x1, y1, x2, y2, has_top_wall, has_right_wall, has_bottom_wall, has_left_wall):
         self.__x1 = x1
@@ -18,27 +13,37 @@ class Cell:
         self.has_top_wall=has_top_wall
         self.has_bottom_wall=has_bottom_wall
 
+    def mid_point(self):
+        x_mid = (self.__x1 + self.__x2) / 2
+        y_mid = (self.__y1 + self.__y2) / 2
+        return Point(x_mid, y_mid)
+
+
+    def draw_move(self, to_cell, undo=False):
+        p1 = self.mid_point()
+        p2 = to_cell.mid_point()
+        self.__win.draw_line(Line(p1, p2), 'grey' if undo else 'red')
+
+    def draw_wall(self, x1, y1, x2, y2):
+        p1 = Point(x1, y1)
+        p2 = Point(x2, y2)
+        line = Line(p1, p2)
+        self.__win.draw_line(line, "black")
+
     def draw(self):
         if self.has_top_wall:
-            p1 = Point(self.__x1, self.__y1)
-            p2 = Point(self.__x2, self.__y1)
-            line = Line(p1, p2)
-            self.__win.draw_line(line, "black")
+            self.draw_wall(self.__x1, self.__y1, self.__x2, self.__y1)
         if self.has_right_wall:
-            p1 = Point(self.__x2, self.__y1)
-            p2 = Point(self.__x2, self.__y2)
-            line = Line(p1, p2)
-            self.__win.draw_line(line, "black")
+            self.draw_wall(self.__x2, self.__y1, self.__x2, self.__y2)
         if self.has_bottom_wall:
-            p1 = Point(self.__x2, self.__y2)
-            p2 = Point(self.__x1, self.__y2)
-            line = Line(p1, p2)
-            self.__win.draw_line(line, "black")
+            self.draw_wall(self.__x2, self.__y2, self.__x1, self.__y2)
         if self.has_left_wall:
-            p1 = Point(self.__x1, self.__y1)
-            p2 = Point(self.__x1, self.__y2)
-            line = Line(p1, p2)
-            self.__win.draw_line(line, "black")
+            self.draw_wall(self.__x1, self.__y1, self.__x1, self.__y2)
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
 class Line:
     def __init__(self, p1, p2):
@@ -90,6 +95,7 @@ def main():
     cell.draw()
     cell1 = Cell(win, 0, 50, 50, 100, False, True, True, False)
     cell1.draw()
+    cell.draw_move(cell1)
     cell2 = Cell(win, 50, 50, 100, 100, True, True, True, True)
     cell2.draw()
     cell3 = Cell(win, 150, 150, 200, 200, True, True, True, True)
